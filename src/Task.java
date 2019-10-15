3903b443c51f0e469b0a522c8098b419b58c53a4
Soraya Behroozian <sorayabehroozian@gmail.com>
2019-10-15 23:08:02 +0200

    The main switch is set up and working.Date needs to be modified.

--- src/Task.java
+++ src/Task.java
@@ -9,11 +9,6 @@ public class Task {
     public  String status;
 
 
-    // public  boolean status;
-
-    public enum Level {
-        Finished, Unfinished ;
-    }
 
     public Task(String item, String taskDescription, Date dueDate, String status) {
 
@@ -25,28 +20,30 @@ public class Task {
 
 
 
-
+    public void setTask(String item) { this.item = item;}
     public String getTask() {
         return item;
     }
 
+    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
     public Date getDueDate() {
         return dueDate;
     }
 
 
-
+    public void setDescription(String taskDescription) { this.taskDescription= taskDescription; }
     public String getDescription() {
      return taskDescription;
     }
 
+    public  void setStatus(String status){
+        this.status = status;
+    }
     public  String getStatus(){
         return status;
     }
 
-    public void createTask(Task task, String item, String taskDescription){
-        return;
-    }
+    //public void createTask(Task task, String item, String taskDescription){return;}
 
 
 
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,10 +1,15 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.text.ParseException;
-import java.util.InputMismatchException;
-import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.io.FileWriter;
+import java.util.Date;
+import java.util.stream.Stream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
 
 
 public class TaskManager {
@@ -12,90 +17,74 @@ public class TaskManager {
 
     private Scanner scanner = new Scanner(System.in);
     ArrayList<Task>  tasks = new ArrayList<>();
-    //Task newTask = new Task();
-    Date dueDate;
 
+  //  Date dueDate;
+    String status;
+
+    //This method displays Welcome Message.
     public void welcomeMessage() {
         System.out.println("Welcome to the To Do List Application");
     }
 
-    public void printInstructions() {
-        System.out.println(" - To see the instructions  \n " +
-                "- Press 0, To print the list of tasks press 1 \n " +
-                "- Press 2, To create a task  \n " +
-                "- Press 3, To modify a task  \n " +
-                "- Press 4, To remove a task  \n " +
-                "- Press 5, To quit  ");
-    }
+
+// Print Instructions
+        public void printInstructions() {
+            System.out.println("" +
+                    " - Press 1, Show task list\n" +
+                    " - Press 2, To add a new task\n" +
+                    " - Press 3, To edit a task\n" +
+                    " - Press 4, To save and quit\n" );
+        }
 
 
 
 
-    public void addTask() {
+
+
+    public void addTask() throws ParseException{
+
+//Adding a task name by the user.
         System.out.println("Please enter a task name: ");
         String item = scanner.nextLine();
-
+// Adding project description
         System.out.println("Please enter description for the task: ");
         String taskDescription = scanner.nextLine();
 
+//Getting the date in a weird range, needs to be restricted
         System.out.println("Please enter the due date for the task in format MM-dd-yyyy: ");
         String date= scanner.nextLine();
-        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-        try {
-            this.dueDate = format.parse(date);
-        } catch (ParseException e) {
-            System.out.println("Parse Exception");
-        }
+        Date dueDate = new SimpleDateFormat("MM-dd-yyyy").parse(date);
+
+//Marking the status of the task and printing an error if one of the two options is not the input.
         System.out.println("Please enter the status of the task:Finished/Unfinished");
-        String status =scanner.nextLine();
+        String statusCheck =scanner.nextLine();
 
-
-        Task.Level statusCheck = Task.Level.Finished;
-        switch(statusCheck) {
-            case Finished:
-
-                System.out.print("Finished Task");
-                break;
-            case Unfinished:
-                System.out.print("Unfinished Task");
-                break;
+        try { if (statusCheck.equals("Finished")){
+             this.status = "Finished";
+        }
+        else if (statusCheck.equals("Unfinished")){
+             this.status = "Unfinished";
+        }
+        else { System.out.println("Invalid input!");
+        }
+        }catch (InputMismatchException e) {
+            System.out.println("Invalid input!");
         }
 
-
-/*
-            try {
-                System.out.print("Is the task already done? true/false");
-                 status = scanner.nextBoolean();
-                if (status == true) {
-                    Task.getStatus(Done);
-                } else if (status == false) {
-                    System.out.println("Task undone");
-                }
-
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input!");
-            }
-
-*/
-
-
-        tasks.add(new Task(item,taskDescription,dueDate,status));
-         //   Task.getTask(item);
-      // newTask.getDescription(taskDescription);
+//Creating the objact in class Task and adding it to the ArrayList.
+        Task newTask = new Task(item,taskDescription,dueDate,status);
+        tasks.add(newTask);
     }
 
+
+//This method removes a task that user has input the title of by searching through the tasks and finding a match. Should I
+//add another criteria to avoid deleting the wrong task? in case of duplicates.
     public void removeTask() {
         System.out.println("Please type in the name of the tax you want to remove");
         String taskToRemove = scanner.nextLine();
 
-       /* if (tasks.contains(taskToRemove)) {
-            int index = tasks.indexOf(taskToRemove);
-            //  String number = tasks.get(index);
-            tasks.remove(tasks.indexOf(taskToRemove));
-            System.out.println(tasks.indexOf(taskToRemove));
-
-        }*/
         Iterator<Task> it = tasks.iterator();
+
         while(it.hasNext()){
             Task s = it.next();
             if(taskToRemove.equals(s.getTask())){
@@ -104,60 +93,178 @@ public class TaskManager {
         }
     }
 
-
-    public void modifyTask() {
+//This method is there to modify the task. All the parameters could be changed.If the user leaves input blank,
+// the value will be the same as it was.
+    public void modifyTask() throws ParseException {
         System.out.println("Please enter the task name you want to replace");
-
         String taskToModify = scanner.nextLine();
 
-        System.out.println("Enter the new task :");
-        //addTask();
-        //tasks.set(tasks.indexOf(taskToModify), new Task());
+        Iterator<Task> it = tasks.iterator();
 
+        while (it.hasNext()) {
+            Task s = it.next();
+            if (taskToModify.equals(s.getTask())) {
+            }
+            System.out.println("Enter the new task name :");
+            String newName = scanner.nextLine();
+            if (newName == null) newName.equals(s.getTask());
+            System.out.println("Please enter new description for the task: ");
+            String newTaskDescription = scanner.nextLine();
+            if (newTaskDescription == null) newTaskDescription.equals(s.getDescription());
+            System.out.println("Please enter the due date for the task in format MM-dd-yyyy: ");
+            String newDate = scanner.nextLine();
+            Date newDueDate = new SimpleDateFormat("MM-dd-yyyy").parse(newDate);
+            if (newDate == null) newDueDate.equals(s.getDueDate());
+            System.out.println("Please enter the status of the task:Finished/Unfinished");
+            String newStatusCheck = scanner.nextLine();
+            if (newStatusCheck == null) status.equals(s.getStatus());
+            try {
+                if (newStatusCheck.equals("Finished")) {
+                    this.status = "Finished";
+                } else if (newStatusCheck.equals("Unfinished")) {
+                    this.status = "Unfinished";
+                } else {
+                    System.out.println("Invalid input!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input!");
+            }
+            Task modifiedTask = new Task(newName, newTaskDescription, newDueDate, status);
+            tasks.set(tasks.indexOf(taskToModify), modifiedTask);
+
+        }
+    }
+
+// This method sorts the tasks by their name.
+    public void sortTasksByName(){
+        List<Task> sortedByTask = tasks.stream().sorted((t1, t2)  ->  t1.getTask().compareTo(t2.getTask())).collect(Collectors.toList());
+        sortedByTask.forEach(Task -> {
+           System.out.println("Task : " +  Task.getTask() + ", Description : " + Task.getDescription()
+            + ", Due Date : " + Task.getDueDate()+ ", Status : " + Task.getStatus() );
+        });
+
+        
+    }
+
+//THis method sorts the tasks by their due date.
+    public void sortTasksByDueDate(){
+        List<Task> sortedByTask = tasks.stream().sorted((t1, t2)  ->  t1.getDueDate().compareTo(t2.getDueDate())).collect(Collectors.toList());
+        sortedByTask.forEach(Task -> {
+           System.out.println(", Due Date : " + Task.getDueDate() + "Task : " +  Task.getTask()
+                   + ", Description : " + Task.getDescription() + ", Status : " + Task.getStatus() );
+        });
     }
 
 
-    public void printArray() {
-       // for (int i = 0; i < tasks.size(); i++) {
-         //  System.out.println(tasks.get(i));
-        tasks.forEach(Task -> {
-                System.out.println("Task : " +  Task.getTask() + ", Description : " + Task.getDescription()
-                        + ", Due Date : " + Task.getDueDate()+ ", Status : " + Task.getStatus() );
-            });
+// This method saves the tasks in a file with name entered by the user.
 
+   public void save() throws FileNotFoundException {
+       try {
+           System.out.println("Enter name of the file :");
+           String fileName = scanner.nextLine();
+
+           FileOutputStream fout = new FileOutputStream(fileName);
+           ObjectOutputStream oos = new ObjectOutputStream(fout);
+           oos.writeObject(tasks);
+           fout.close();
+       }catch(IOException ioException) {
+            ioException.printStackTrace();
+       }
+   }
+
+//This method counts the number of finished and unfinished tasks.
+    public void statusCount(){
+        // Task taskStream = tasks.stream()
+       long countFinished =  tasks.stream().filter(x -> "Finished".equals(x.getStatus())).count();
+       long countUnfinished = tasks.size() - countFinished;
+       System.out.println("You have "+ countFinished + " tasks done and " + countUnfinished + " tasks undone!");
     }
 
-
-//+ Task.getStatus()
-
-    public void TaskManager() {
-
+    
+// This method gives the choice to either update or remove a task by bringing two methods under a switch.
+       public void updateTask(){
         boolean quit = false;
         int choice = 0;
+          while (!quit) {
+              System.out.println("Enter 1 to update, 2 to remove a task and 3 to return to main menue: ");
+              choice = scanner.nextInt();
+              scanner.nextLine();
+              switch (choice) {
+                  case 1:
+                      try {
+                          sortTasksByName();
+                          modifyTask();
+                      } catch (ParseException e) {
+                          e.printStackTrace();
+                      }
+                      break;
+                  case 2:
+                      sortTasksByName();
+                      removeTask();
+                  case 3:
+                      quit = true;
+                      printInstructions();
+
+              }
+          }
+       }
+
+//This method gives the choice wether to print tasks sorted by name or by due date, using a switch.
+    public void showTaskList() {
+       boolean quit = false;
+       int choice = 0;
+         while (!quit) {
+             System.out.println("Enter 1 to sort by name, 2 to sort by due date and 3 to return to main menue: ");
+             choice = scanner.nextInt();
+             scanner.nextLine();
+             switch (choice) {
+                 case 1:
+                     sortTasksByName();
+                     break;
+                 case 2:
+                     sortTasksByDueDate();
+                 case 3:
+                     quit = true;
+                     printInstructions();
+             }
+         }
+    }
+
+
+
+
+
+//The main switch, user can choose 4 actions.
+    public void TaskManager() throws ParseException {
+
+        boolean quit = false;
+        int choice = 1;
         welcomeMessage();
         printInstructions();
         while (!quit) {
+            statusCount();
             System.out.println("Enter your choice: ");
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
-                case 0:
-                    printInstructions();
-                    break;
                 case 1:
-                    printArray();
+                    showTaskList();
                     break;
+
                 case 2:
                     addTask();
+                    break;
 
-                    break;
                 case 3:
-                    modifyTask();
+                    updateTask();
                     break;
+
                 case 4:
-                    removeTask();
-                    break;
-                case 5:
+                    try {
+                        save();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
                     quit = true;
                     break;
             }
